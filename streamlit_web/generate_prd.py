d53a697f625b60e670811597996f5ec19b4f5ea0
AryaStark13 <arihant.sheth0802@gmail.com>
2023-06-08 14:10:33 +0530

    added a Streamlit web interface

---
+++ streamlit_web/generate_prd.py
@@ -0,0 +1,39 @@
+import langchain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate, load_prompt
+import wandb
+from wandb.integration.langchain import WandbTracer
+import openai
+
+openai.api_key = "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"
+
+def generate_prd(company_name, company_desc, existing_feature_list, new_feature, new_feature_desc, wandb_name):
+    wandb.init(
+        project="generate_simple_prd",
+        config={
+            "model": "gpt-3.5-turbo",
+            "temperature": 0
+        },
+        entity="arihantsheth",
+        name=wandb_name,
+    )
+
+    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    prompt_template = load_prompt(r"C:\Users\ariha\Desktop\Synap Labs\prompt_templates\generate_prd_template.json")
+
+    prompt = prompt_template.format(company_name=company_name, company_desc=company_desc,
+                                    existing_feature_list=existing_feature_list, new_feature=new_feature, new_feature_desc=new_feature_desc)
+
+    try:
+        output = llm(prompt, callbacks=[WandbTracer()])
+    except openai.error.AuthenticationError as e:
+        print("OpenAI unknown authentication error")
+        print(e.json_body)
+        print(e.headers)
+        return
+
+    with open(fr"C:\Users\ariha\Desktop\Synap Labs\generated_prds\{company_name}_prd_{new_feature}.md", "w") as f:
+        f.write(output)
+
+    wandb.finish()
+    return output
---
+++ streamlit_web/main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import json
+import time
+from generate_prd import generate_prd
+
+def get_prd(company_name, company_desc, existing_feature_list, new_feature, new_feature_desc):
+    wandb_name = f"{company_name}_prd_{new_feature}"
+    start_time = time.time()
+    output = generate_prd(company_name, company_desc, existing_feature_list,
+                          new_feature, new_feature_desc, wandb_name)
+    end_time = time.time()
+
+    total_time = str(int(end_time - start_time))
+    return output, total_time
+
+def get_company_info(company_name):
+    with open("company_info.json", "r") as f:
+        company_info = json.load(f)[company_name]
+
+    return company_info
+
+
+def main():
+    dropdown_to_company_name = {
+        "Weights and Biases": "wandb",
+        "Langchain": "langchain",
+    }
+
+    # Create a dropdown selectbox
+    dropdown_select = st.selectbox(
+        "Select an option:", ("Weights and Biases", "Langchain", "Other"))
+    
+    if dropdown_select == "Other":
+        company_info = {
+            "company_name": st.text_input("Company Name:"),
+            "company_desc": st.text_input("Company Description:"),
+            "existing_feature_list": st.text_area("Existing Feature List:"),
+        }
+        company_name = company_info["company_name"]
+    else:
+        company_name = dropdown_to_company_name[dropdown_select]
+        company_info = get_company_info(company_name)
+
+    # Create text input fields for feature name and description
+    feature_name_input = st.text_input(
+        "Feature Name:", value="Model Explainability Toolkit (MXT)")
+    feature_description_input = st.text_input(
+        "Feature Description:", value="Enables users to interpret and explain the outputs and decision-making of their AI Models.")
+
+    # Create a button and check if both text input fields are not empty before enabling it
+    if st.button("Get PRD", disabled=not (feature_name_input and feature_description_input)):
+        with st.spinner(text="Generating PRD..."):
+            if 'output' not in st.session_state:
+                st.session_state.output, st.session_state.total_time = get_prd(**company_info, new_feature=feature_name_input,
+                                                                               new_feature_desc=feature_description_input)
+                st.session_state.edited_output = st.session_state.output
+
+    if 'output' in st.session_state and 'total_time' in st.session_state and 'edited_output' in st.session_state:
+        st.write(f"Time taken: {st.session_state.total_time} seconds")
+
+        st.download_button(label="Download PRD", data=st.session_state.edited_output,
+                           file_name=f"{company_name}_prd_{feature_name_input}.md", mime="text/markdown")
+
+        if st.button("Edit PRD"):
+            while not st.button("Finished Editing"):
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.text("Markdown Editor")
+                    st.session_state.edited_output = st.text_area(
+                        label="Markdown Editor", value=st.session_state.output, height=500, label_visibility="hidden")
+                with col2:
+                    st.text("Live Preview")
+                    st.markdown(st.session_state.edited_output,
+                                help="Generated PRD")
+
+        st.markdown(st.session_state.edited_output, help="Generated PRD")
+
+
+if __name__ == "__main__":
+    main()
